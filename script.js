fa0d139607219a3e2e17529649b40332b22d1171
Yacine <yacinewkka@gmail.com>
2024-05-30 11:27:15 +0100

    get request using fetch and using Axios

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function getUsers(){
-    let request = new XMLHttpRequest();
-    request.open('GET','https://jsonplaceholder.typicode.com/users') ;
-    request.setRequestHeader('Content-type','application/json');
-    request.responseType = 'json';
-    request.send();
-    request.onload = function(){
-        if(request.status >= 200 && request.status < 300){
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+            if(response.ok){
+                return response.json();
+            }
+        })
+        .then(users => {
             let usersElm = document.querySelector('.users');
-            let users = request.response;
             for(usr in users){
                 let li = document.createElement('li');
                 let nameElm = document.createElement('span') ;
@@ -33,39 +32,83 @@ function getUsers(){
                     getPostes(dataIdUser);
                 });
             }); 
-        }
-        else{
-            alert("error");
-        }
-        
-    }
-    
+        })
 }
 function getPostes(val){
-    let request = new XMLHttpRequest();
-    request.open('GET',`https://jsonplaceholder.typicode.com/posts?userId=${val}`) ;
-    request.setRequestHeader('Content-type','application/json');
-    request.responseType = 'json';
-    request.send();
-    request.onload = function(){
-        if(request.status >= 200 && request.status < 300){
-            let response = request.response;
+    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${val}`)
+        .then(response => {
+            if(response.ok){
+                return response.json();
+            }
+        })
+        .then(posts => {
             let postsElm = document.querySelector('.posts');
             postsElm.innerHTML = '';
-            for(post in response){
+            for(post in posts){
                 let postData = document.createElement('div');
                 let title = document.createElement('h4');
                 let text = document.createElement('p');
                 postData.setAttribute("class","post");
-                title.appendChild(document.createTextNode(response[post].title));
-                text.appendChild(document.createTextNode(response[post].body));
+                title.appendChild(document.createTextNode(posts[post].title));
+                text.appendChild(document.createTextNode(posts[post].body));
                 postData.appendChild(title);
                 postData.appendChild(text);
                 postsElm.appendChild(postData);
             }
-        }else{
-            alert("error");
-        }
-    }
+        })
 }
-getUsers();
+function gitUsersUsingAxios(){
+    axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+            let users = response.data;
+            let usersElm = document.querySelector('.users');
+            for(usr in users){
+                let li = document.createElement('li');
+                let nameElm = document.createElement('span') ;
+                let emailElm = document.createElement('span') ;
+                li.setAttribute("class", "user") ;
+                li.setAttribute("data-id", users[usr].id) ;
+                nameElm.appendChild(document.createTextNode(users[usr].name));
+                emailElm.appendChild(document.createTextNode(users[usr].email));
+                li.appendChild(nameElm);
+                li.appendChild(emailElm);
+                usersElm.appendChild(li);
+            } 
+            let userS = document.querySelectorAll('.user');
+            document.querySelector('.users li:first-child').classList.add('active');
+            getPostsUsingAxios(1);
+            userS.forEach(function (usR) {
+                usR.addEventListener('click', function () {
+                    userS.forEach((e)=>{
+                        e.classList.remove('active');
+                    })
+                    usR.classList.toggle('active');
+                    let dataIdUser = usR.getAttribute('data-id');
+                    getPostsUsingAxios(dataIdUser);
+                });
+            });
+        })
+        .catch(err => alert(err));
+}
+function getPostsUsingAxios(val){
+    axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${val}`)
+        .then(response => {
+            let posts = response.data;
+            let postsElm = document.querySelector('.posts');
+            postsElm.innerHTML = '';
+            for(post in posts){
+                let postData = document.createElement('div');
+                let title = document.createElement('h4');
+                let text = document.createElement('p');
+                postData.setAttribute("class","post");
+                title.appendChild(document.createTextNode(posts[post].title));
+                text.appendChild(document.createTextNode(posts[post].body));
+                postData.appendChild(title);
+                postData.appendChild(text);
+                postsElm.appendChild(postData);
+            }
+        })
+        .catch(err => alert(err));
+}
+//getUsers();
+gitUsersUsingAxios();
